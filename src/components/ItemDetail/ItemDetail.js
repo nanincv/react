f82b1ac8a05f769ef09396c463ff63c9477bb70a
Naty Martorelli <nmartorelli@gmail.com>
2022-09-08 19:38:30 -0300

    agrego link botón

--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -5,6 +5,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 
+
 function ItemDetail( {data} ) {
   return (
     <div >
--- src/components/ItemDetailContainer/ItemDetailContainer.js
+++ src/components/ItemDetailContainer/ItemDetailContainer.js
@@ -1,19 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ItemDetail  from '../ItemDetail/ItemDetail';
+import { useParams } from 'react-router-dom';
 
 
 const ItemDetailContainer = () => {
 	const [item, setItem] = useState([]);
 
+	let { id } = useParams();
+
 	useEffect(() => {
-		fetch('https://fakestoreapi.com/products/5')
+		fetch(`https://fakestoreapi.com/products/${id}`)
 			.then((response) => response.json())
 			.then((json) => setItem(json));
-	}, []);
+	}, [id]);
 
 	return (
-        <div>
+        <div className="d-flex justify-content-center">
 			 <ItemDetail data={item} />;
         </div>
 
