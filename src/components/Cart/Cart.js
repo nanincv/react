fda42ff9eb70014aed92b629f4b70c5c99c504de
Naty Martorelli <nmartorelli@gmail.com>
2022-10-05 09:21:28 -0300

    orders

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,12 +2,52 @@ import React from 'react';
 import useCartContext from '../../Context/CartContext';
 import { Link } from 'react-router-dom';
 import { Button } from 'react-bootstrap';
+import { doc, setDoc, serverTimestamp, collection } from "firebase/firestore";
+import firestoreDB from '../../Firebase/firebaseConfig';
+
+
+
 //import Table from 'react-bootstrap/Table';
+
+
 
 
 const Cart = () => {
  const {cart, removeFromCart, clearCart, totalPriceCart} = useCartContext();
- console.log ("CART", cart);
+
+
+ // ordenes
+
+ const createOrder = async () => {
+  let itemsForDB = cart.map((item) => ({
+    id: item.id,
+    title: item.name,
+    price: item.price,
+    quantity: item.cant,
+}));
+  const order = {
+    buyer: {
+    name: "Natalia", 
+    phone: "555-5555", 
+    email: "nmartorelligmail.com"
+    },
+    date: serverTimestamp (),
+    items: itemsForDB,
+    total: totalPriceCart(),
+  }
+ // console.log (order)
+  const newOrderRef = doc(collection(firestoreDB, "orders")) 
+  await setDoc(newOrderRef, order);
+  alert ( 'Se ha creado tu orden con el ID:' + newOrderRef.id );
+  clearCart();
+
+
+};
+
+
+
+// cart
+
  
  if (cart.length === 0) {
   return <div style= {{textAlign:"center", margin: "50vh"}}>
@@ -29,11 +69,14 @@ else {
       </div>
     })}
       <h5>Total a pagar: ${totalPriceCart().toFixed(2)}{" "}</h5>
-      <Button onClick={clearCart} >Vaciar Carrito</Button>
+      <Button onClick={clearCart}>Vaciar Carrito</Button>
+      <Button onClick={createOrder}>Check  out</Button>
       </div>
+      
 
   
   }//cierro else
 }//cierro Cart
 
 export default Cart;
+
